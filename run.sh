a261e1b8453c440b291b8a4ef1d38767042fdc6d
Kevin Lin <kevinptt0323@gmail.com>
2017-10-23 15:43:05 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-$DIR = rcfiles
+DIR=rcfiles
 cd $HOME
 git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
-ln -s $DIR/.zshrc-omg .zshrc
 ln -s $DIR/.gitconfig .
+ln -s $DIR/.tmux.conf .
+ln -s $DIR/.zshrc-omg .zshrc
