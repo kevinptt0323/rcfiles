9ea6298cdcf75e651ceb06d88858689a4714399f
Kevin Lin <kevinptt0323@gmail.com>
2018-04-14 19:07:37 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
 ln -s $RC_DIR/.gitconfig .
 ln -s $RC_DIR/tmux.conf .tmux.conf
 mv .zshrc .zshrc-bak
-echo "RC_DIR=$RC_DIR\n\
-source \$RC_DIR/zshrc.sh" > .zshrc
+
+cat <<EOT > .zshrc
+RC_DIR=$RC_DIR
+source \$RC_DIR/zshrc.sh
+EOT
