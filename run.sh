716d5871bbd0c5e64191e3bd5559dae2a7a9aaab
Kevin Lin <kevinptt0323@gmail.com>
2018-04-14 19:08:31 +0800

    use cp to backup .zshrc

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd $HOME
 git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
 ln -s $RC_DIR/.gitconfig .
 ln -s $RC_DIR/tmux.conf .tmux.conf
-mv .zshrc .zshrc-bak
+cp .zshrc .zshrc-bak
 
 cat <<EOT > .zshrc
 RC_DIR=$RC_DIR
